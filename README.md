0ea92abd559148fa539b8bfa4a9f66735d293af8
jennylc7 <thepilatesnut@gmail.com>
2021-04-26 13:10:16 -0600

    fourth push

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
-# README-generator
-A README that prompts the user to enter in data.
+# Welcome to A Good README 👋
+[![Version](https://img.shields.io/npm/v/A Good README.svg)](https://www.npmjs.com/package/A Good README)
+[![Documentation](https://img.shields.io/badge/documentation-yes-brightgreen.svg)](https://github.com/Jennylc7/README-generator#readme)
+[![Maintenance](https://img.shields.io/badge/Maintained%3F-yes-green.svg)](https://github.com/Jennylc7/README-generator/graphs/commit-activity)
+[![License: ISC](https://img.shields.io/github/license/Jennylc7/A Good README)](https://github.com/Jennylc7/README-generator/blob/master/LICENSE)
+
+> A README that prompts the user to enter in data.
+
+### 🏠 [Homepage](https://github.com/Jennylc7/README-generator#readme)
+
+## Install
+
+```sh
+npm install
+```
+
+## Run tests
+
+```sh
+npm run test
+```
+
+## Author
+
+👤 **Jennifer Clayton**
+
+* Website: www.thepilatesnut.com
+* Github: [@Jennylc7](https://github.com/Jennylc7)
+
+## 🤝 Contributing
+
+Contributions, issues and feature requests are welcome!
+
+Feel free to check [issues page](https://github.com/Jennylc7/README-generator/issues). You can also take a look at the [contributing guide](https://github.com/Jennylc7/README-generator/blob/master/CONTRIBUTING.md).
+
+## Show your support
+
+Give a ⭐️ if this project helped you!
+
+
+## 📝 License
+
+Copyright © 2021 [Jennifer Clayton](https://github.com/Jennylc7).
+
+This project is [ISC](https://github.com/Jennylc7/README-generator/blob/master/LICENSE) licensed.
+
+***
+_This README was generated with ❤️ by [readme-md-generator](https://github.com/kefranabg/readme-md-generator)_
--- index.js
+++ index.js
@@ -1,1 +1,148 @@
 //This is my new index.js
+
+// TODO: Include packages needed for this application
+
+
+
+
+
+
+
+
+
+//prompt- questions array for the user
+const inquirer = require(inquirer);
+const fs = require("fs");
+const { DH_NOT_SUITABLE_GENERATOR } = require("node:constants");
+console.log("```");
+
+// TODO: Create an array of questions for user input
+//prompt- questions array for the user
+const questions = ([
+    {
+        //GitHub username
+        message: "Please enter in your Github username.",
+        name: "username",
+        type: "input",
+    },
+    {
+        //email address
+        message: "Please enter in your email address.",
+        name: "email",
+        type: "input",
+    },
+    {
+        //Project title
+        message: "What would you like your project title to be?",
+        name: "title",
+        type: "input",
+    },
+    
+    {
+    //project description
+        message: "Please provide a brief description of your project.",
+        name: "description",
+        type: "input",
+    },
+    {
+        //installation
+        message: "Please provide any installation instructions for your project.",
+        name: "installation",
+        type: "input",
+    },
+    {
+        //usage
+        message: "How does the user run this program.",
+        name: "usage",
+        type: "input",
+    },
+    {
+        //license
+        message: "What kind of license would you like your program to have?",
+        name: "license",
+        type: "checkbox",
+        choices: ["Apache",
+                  "MIT",
+                  "ISC"] 
+    },
+  
+    {
+        //contributors
+        message: "Who are the contributors to your project?",
+        name: "contributors",
+        type: "input",
+    },
+    {
+        //tests
+        message: "Run tests here.",
+        name: "tests",
+        type: "input",
+    },
+    {
+        //project description
+        message: "Please provide a brief description of your project.",
+        name: "description",
+        type: "input",
+    },
+]);
+
+const READMEfile = `# $(title)
+[Github license($badge)]`
+
+`[username] (#username)`
+`[email] (#email)`
+`[title] (#title)`
+`[description] (#description)` 
+`[installation]  {#installation)` 
+`[usage]  (#usage)`
+`[license]  (#license)`
+`[contributors] (#contributors)` 
+`[tests]  (#tests)` 
+`[description]  (#description)`
+
+
+
+`##Username`
+`${username}`
+
+`##Email`
+`${email}`
+
+`##Title`
+`${title}`
+
+`##Description`
+`${description}`
+
+`##Installation`
+
+`##Usage`
+
+`##License`
+
+`##Contributors`
+
+`##Tests`
+
+`##Description`
+
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+
+
+
+// TODO: Create a function to initialize app
+function init() {
+    inquirer
+    .prompt(questions)
+    .then(function(response) {
+        writeToFile("README.generator.md", response);
+    })
+}
+
+
+// Function call to initialize app
+init();
+
